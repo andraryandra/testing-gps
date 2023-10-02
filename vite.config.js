5b3703d43462936cf25c157880c5db38c6af0edb
unknown <andraryandra38@gmail.com>
2023-10-02 10:39:12 +0700

    delete build in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    build: {
-        outDir: "dist",
-    },
     plugins: [
         laravel({
             input: ["resources/sass/app.scss", "resources/js/app.js"],
